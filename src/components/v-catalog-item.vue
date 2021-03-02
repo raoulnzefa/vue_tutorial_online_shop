bc5d4e3d099aaa3511387142a226e8b2481242ac
Dmitrij Lukashenko <Lukdmitrij@yandex.com>
2021-03-03 00:20:28 +0200

    json-data was attached

---
+++ src/components/v-catalog-item.vue
@@ -0,0 +1,32 @@
+<template>
+ <div class="v-catalog-item">
+   <img src="" alt="catalog img item">
+   <p class="v-catalog-item__name">{{}}</p>
+   <p class="v-catalog-item__price">price: 100</p>
+   <button class="v-catalog-item__add_to_cart_btn btn">Add to card</button>
+ </div>
+</template>
+
+<script>
+export default {
+name: "v-catalog-item",
+  props: {
+    product__data : {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/styles.scss";
+  .v-catalog-item {
+    flex-basis: 25%;
+    box-shadow: 0 0 8px 0 #e0e0e0;
+    padding: $padding*2;
+    margin-bottom: $margin*2;
+  }
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="v-main-wrapper">
+    <p>{{title}}</p>
+    <v-catalog/>
+    <v-card/>
+  </div>
+</template>
+
+<script>
+import vCatalog from './v-catalog'
+import vCard from './v-card'
+export default {
+  name: "v-main-wrapper.vue",
+  components: {
+    vCatalog,
+    vCard
+},
+  props: {},
+  data() {
+    return {
+      title :"Main"
+    }
+  },
+  computed: {
+
+  },
+  methods: {},
+  watch: {},
+  mounted() {
+    console.log('Hello i am alive')
+  }
+}
+</script>
+
+<style scoped>
+.v-main-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 900px;
+  margin: 0 auto;
+}
+</style>
